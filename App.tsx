abd975ae608761eeeba1253c78681a7cfb6b4328
LarsVDD05 <vandendaelelars@gmail.com>
2026-01-05 13:39:48 +0100

    added folders

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>MiniShop - React Native App</Text>
+      <Text>Project opgestart!</Text>
       <StatusBar style="auto" />
     </View>
   );
